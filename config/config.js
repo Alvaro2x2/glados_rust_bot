c0cef92b251a9bbf50f9d70e97937e440fdb7507
Alvaro Zaragoza <xx629@globalia.com>
2022-12-10 19:25:26 +0100

    l

---
+++ config/config.js
@@ -0,0 +1,59 @@
+// ============================
+//  General Properties
+// ============================
+let get = {
+    server: {
+        port: 28082,
+        ip: '188.213.7.97'
+
+    },
+    user : {
+        steamid: '76561198044832194',
+        token: '-1419384213'
+    },
+    dispositive: {
+        sam : '67112',
+        
+    },
+    cupboard : {
+        tc1 : '67787',
+        tc2 : ''
+    },
+    schedule: {
+        cargo : '5',
+        time : '5',
+        smallpetro : '5',
+        bigpetro : '5'
+    },
+    craftCost: {
+        rocket: {
+            sulfur : 0,
+            gunpowder : 150,
+            explosive : 10
+        },
+        satchel : {
+            gunpowder : 240,
+        },
+        c4 : {
+            explosive : 20,
+            techtrash : 2
+        },
+        explosiveammo : {
+            gunpowder : 10,
+            sulfur : 5,
+            lowgrade : 20
+        },
+        highvelocityrocket : {
+            gunpowder : 100
+        },
+        explosive : {
+            sulfur : 10,
+            gunpowder : 50
+        },
+        gunpowder : {
+            sulfur : 2
+        }
+    }
+};
+
+module.exports = get;
